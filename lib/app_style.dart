ae1ff17c6697a905b0ee87aa364216b4826d6708
Olayiwolaaa <muizzkara91@gmail.com>
2022-11-16 17:17:20 +0100

    Beginning of Ui

---
+++ lib/app_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kWhite = Color.fromARGB(0, 255, 255, 255);
+// color
+const Color kBlack = Color(0x0000);
+const Color kGrey100 = Color.fromARGB(6, 177, 177, 190);
+const Color kGrey200 = Color.fromARGB(0, 234, 233, 238);
+const Color kGrey500 = Color.fromARGB(0, 124, 124, 124);
+const Color kGrey600 = Color.fromARGB(0, 32, 32, 32);
+// text style
+final kMulishBold = GoogleFonts.mulish(fontSize: 20, fontWeight: FontWeight.w700);
+final kMulishSemibold = GoogleFonts.mulish(fontWeight: FontWeight.w600);
+final kMulishMedium = GoogleFonts.mulish(fontWeight: FontWeight.w500);
+final kMulishRegular = GoogleFonts.mulish(fontWeight: FontWeight.w400, color: Colors.grey[500]);
